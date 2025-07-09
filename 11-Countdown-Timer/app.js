2b2396f9bd967b369d46d1180e61364488d1d6e5
tarun-102 <tarungohil6969@gmail.com>
2025-07-09 16:10:06 +0530

    create the project and add js

---
+++ 11-Countdown-Timer/app.js
@@ -0,0 +1,68 @@
+// Select all elements
+const hour = document.querySelector('#hour');
+const minute = document.querySelector('#minute');
+const second = document.querySelector('#second');
+
+const display = document.querySelector('span');
+const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
+const resetBtn = document.querySelector('#reset');
+
+let totalSeconds = 0;
+let timer;
+let isRunning = false;
+
+const updateDisplay = () => {
+    let h = Math.floor(totalSeconds / 3600);
+    let m = Math.floor((totalSeconds % 3600) / 60)
+    let s = totalSeconds % 60;
+
+    let hh = h < 10 ? '0' + h : h;
+    let mm = m < 10 ? '0' + m : m;
+    let ss = s < 10 ? '0' + s : s;
+    display.innerText = `${hh}:${mm}:${ss}`
+}
+
+const startTimer = () => {
+    if(isRunning) return;
+
+    if(totalSeconds === 0) {
+        let h = parseInt((hour.value) || 0)
+        let m = parseInt((minute.value) || 0) 
+        let s = parseInt((second.value) || 0) 
+        totalSeconds = h * 3600 + m * 60 + s;
+    }
+    if (totalSeconds <= 0) return;
+    isRunning = true
+
+    timer = setInterval(() => {
+        if (totalSeconds > 0) {
+            totalSeconds--
+            updateDisplay()
+        }else{
+            clearInterval(timer)
+            isRunning = false;
+            alert("⏰ Time's up!");
+        }
+    } ,1000)
+}
+
+const stopTimer = () => {
+    clearInterval(timer);
+    isRunning = false; 
+}
+
+const resetTimer = () => {
+    clearInterval(timer);
+    isRunning = false;
+    updateDisplay();
+    totalSeconds = 0;
+    hour.value = "";
+    minute.value = "";
+    second.value = "";
+}
+
+startBtn.addEventListener('click', startTimer)
+stopBtn.addEventListener('click', stopTimer)
+resetBtn.addEventListener('click', resetTimer)
+updateDisplay();
